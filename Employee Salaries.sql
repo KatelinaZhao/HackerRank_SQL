e39045bfae37bc618495151d712061ed814efa1b
KatelinaZhao <88176498+KatelinaZhao@users.noreply.github.com>
2022-02-20 16:09:31 -0500

    Update Employee Salaries.sql

--- Employee Salaries.sql
+++ Employee Salaries.sql
@@ -1,1 +1,7 @@
 
+# We want to print the list of employee names which sorted by employee_id, and the employee need have a salary greater than $2000 per month who have been employees for less than  months.
+
+
+select name from Employee
+where salary > 2000 and months < 10
+order by employee_id
