e1b68f8cd4af93d7276abf422b62c95d196a8d39
KatelinaZhao <88176498+KatelinaZhao@users.noreply.github.com>
2022-02-20 18:08:44 -0500

    Update Type of Triangle.sql

--- Type of Triangle.sql
+++ Type of Triangle.sql
@@ -1,13 +1,8 @@
-   Enter your query here and follow these instructions:
-    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
-    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
-    3. Type your code immediately after comment. Don't leave any blank line.
-    
-    We need check A, B and C form which typle of triangle:
-    1) check all the Not A Trigngle situation by check if there is any sum of 2 sides length smaller or equal to the third line length.
-    2) Check if A,B and C could form a Equilateral
-    3) Check any 2 lines are equal to form a Isosceles
-    4) the else situations are belong to Scalene
+#   We need check A, B and C form which typle of triangle:
+#   1) check all the Not A Trigngle situation by check if there is any sum of 2 sides length smaller or equal to the third line length.
+#   2) Check if A,B and C could form a Equilateral
+#   3) Check any 2 lines are equal to form a Isosceles
+#   4) the else situations are belong to Scalene
 
 
 select
@@ -19,3 +14,5 @@ case
     end from TRIANGLES;
                     
     
+#  The CASE statement goes through conditions and returns a value when the first condition is met (like an if-then-else statement). So, once a condition is true, it will stop reading and return the result. If no conditions are true, it returns the value in the ELSE clause.
+#  If there is no ELSE part and no conditions are true, it returns NULL.
