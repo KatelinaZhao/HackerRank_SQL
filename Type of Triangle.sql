d6fa2bd2b299e4620909f1da8f569a6a0cdeeb03
KatelinaZhao <88176498+KatelinaZhao@users.noreply.github.com>
2022-02-20 18:09:08 -0500

    Update Type of Triangle.sql

--- Type of Triangle.sql
+++ Type of Triangle.sql
@@ -14,5 +14,6 @@ case
     end from TRIANGLES;
                     
     
-#  The CASE statement goes through conditions and returns a value when the first condition is met (like an if-then-else statement). So, once a condition is true, it will stop reading and return the result. If no conditions are true, it returns the value in the ELSE clause.
+#  The CASE statement goes through conditions and returns a value when the first condition is met (like an if-then-else statement). 
+#  So, once a condition is true, it will stop reading and return the result. If no conditions are true, it returns the value in the ELSE clause.
 #  If there is no ELSE part and no conditions are true, it returns NULL.
